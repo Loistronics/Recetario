9bd0da1a6dc4a719e8c54f61c520e80d6c1a8db5
Luis Eduardo <loistronics@gmail.com>
2023-12-05 23:41:02 -0400

    Funcion lectura de Receta
    Se agrega la funcion para leer una receta. Las recetas son archivos que estan dentro de carpetas categorias

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import shutil  #Libreria que permite eliminar un directorio con contenido
 import contenidoArchivos as ca
-import importlib
+from os import system
 
 def crear_directorios():
     directorio = Path(Path.home(),"Recetas")
@@ -27,6 +27,36 @@ def crear_archivos(directorio,estructura_inicial):
             archivo.write(receta)
             archivo.close()
 
+def bienvenida(directorio):
+    categorias = os.listdir(directorio)
+    print(f"""                          BIENVENIDO A TU RECETARIO
+        La ruta donde a sido creada la carpeta es {directorio}
+Actualmente el recetario cuenta con las siguientes categorias: {categorias}
+    """)
+
+def leer_receta(directorio):
+    system("cls")
+    categorias = os.listdir(directorio)
+    for i in enumerate(categorias):
+        print(f"{i[0] + 1} - {i[1]}")
+    opcion = input("Escribe el nombre de alguna de las categorias: ")
+    if Path(directorio,opcion).exists():
+        system("cls")
+        recetas = os.listdir(Path(directorio,opcion))
+        for i in enumerate(recetas):
+            print(f"{i[0] + 1} - {i[1]}")
+        archivo = input("Escribe el nombre del archivo de la receta: ")
+        receta_archivo = open(Path(directorio,opcion,archivo))
+        print(receta_archivo.read())
+        receta_archivo.close()
+    else:
+        print("Opcion INCORRECTA")
+        system("cls")
+        leer_receta(directorio)
+
+
+
+
 #El diccionario a continuacion es la estructura inicial las claves son subdirectorios de Recetas y los valores son archivos
 estructura_inicial = {
     "Carnes":["recetaEtrecotAlMalbec", "recetaMatambrePizza"],
@@ -36,3 +66,13 @@ estructura_inicial = {
 }
 raiz_recetas = crear_directorios()
 crear_archivos(raiz_recetas,estructura_inicial)
+bienvenida(raiz_recetas)
+while True:
+    opcion = input("Por favor introduce una opcion: ")
+    match opcion:
+        case "1":
+            leer_receta(raiz_recetas)
+        case  "6":
+            break
+        case _:
+            print("Por Favor Introduzca una opcion CORRECTA")
